302a56facf97f9e0fba8dcac9ced049a4d5a3c66
jiang.he <573748150jh@163.com>
2021-05-26 23:15:23 +0800

    feat(): 调试输出目录修改

--- example/debug/index.js
+++ example/debug/index.js
@@ -2,6 +2,6 @@ const { rollup } = require('@jianghe/rollup');
 
 console.log(`输入：${__dirname}/demo/main.js`);
 rollup(`${__dirname}/demo/main.js`).then((res) => {
-  console.log(`输出：${__dirname}/demo/dist/bundle.js`);
-  res.write(`${__dirname}/demo/bundle.js`);
+  console.log(`输出：${__dirname}/dist/bundle.js`);
+  res.write(`${__dirname}/dist/bundle.js`);
 });
